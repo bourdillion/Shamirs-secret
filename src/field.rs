ef9c1072e4157bebc6cfe39bc26b06c928c7853b
bourdillion <isaacakhigbe22@gmail.com>
2026-04-27 11:22:11 +0100

    chore: finish constructing simple field

--- src/field.rs
+++ src/field.rs
@@ -1,3 +1,5 @@
+use rand::RngExt;
+
 pub trait Field: Sized {
     fn add(self, x: Self) -> Self;
 
@@ -12,9 +14,71 @@ pub trait Field: Sized {
         self.multiply(x_inverse)
     }
 
-    fn zero() -> Self;
+    fn zero(x: u64) -> Self;
 
-    fn one() -> Self;
+    fn one(x: u64) -> Self;
 
     fn random(x: u64) -> Self;
+}
+
+struct SimpleField {
+    value: u64,
+    prime: u64,
+}
+
+impl Field for SimpleField {
+    fn add(self, x: Self) -> Self {
+        let value_added = (self.value + x.value) % self.prime;
+        let ans = SimpleField {
+            value: value_added,
+            prime: self.prime,
+        };
+        ans
+    }
+
+    fn subtract(self, x: Self) -> Self {
+        let value_sub = (self.value + self.prime - x.value) % self.prime;
+
+        SimpleField {
+            value: value_sub,
+            prime: self.prime,
+        }
+    }
+
+    fn mul_inverse(self) -> Self {
+        SimpleField { value: 1, prime: 1 }
+    }
+
+    fn multiply(self, x: Self) -> Self {
+        let value_mul: u64 = (self.value * x.value) % self.prime;
+        SimpleField {
+            value: value_mul,
+            prime: self.prime,
+        }
+    }
+
+    fn one(prime: u64) -> Self {
+        SimpleField {
+            value: 1,
+            prime: prime,
+        }
+    }
+
+    fn random(x: u64) -> Self {
+        let mut rnd_gen = rand::rng();
+        let max = x - 1;
+        let rnd = rnd_gen.random_range(0..=max);
+
+        SimpleField {
+            value: rnd,
+            prime: x,
+        }
+    }
+
+    fn zero(prime: u64) -> Self {
+        SimpleField {
+            value: 0,
+            prime: prime,
+        }
+    }
 }
