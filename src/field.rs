6c8ea0b04bf5ed41afa420d6e164fc12db06256b
bourdillion <isaacakhigbe22@gmail.com>
2026-04-27 10:07:49 +0100

    chore: setup the field trait

--- src/field.rs
+++ src/field.rs
@@ -1,4 +1,4 @@
-pub trait Field {
+pub trait Field: Sized {
     fn add(self, x: Self) -> Self;
 
     fn subtract(self, x: Self) -> Self;
@@ -7,11 +7,14 @@ pub trait Field {
 
     fn mul_inverse(self) -> Self;
 
-    fn division(&self, x: Self) -> Self;
+    fn division(self, x: Self) -> Self {
+        let x_inverse = x.mul_inverse();
+        self.multiply(x_inverse)
+    }
 
     fn zero() -> Self;
 
     fn one() -> Self;
 
-    fn random(self) -> Self;
+    fn random(x: u64) -> Self;
 }
