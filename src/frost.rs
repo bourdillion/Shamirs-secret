a206438ef8c344f782548b4204efbbdee81a40d6
bourdillion <isaacakhigbe22@gmail.com>
2026-05-03 20:50:05 +0100

    chore: add generate function to create signer notice

--- src/frost.rs
+++ src/frost.rs
@@ -1,3 +1,4 @@
+use crate::schnorr::compute_challenge;
 use ark_bls12_381::{Fr, G1Projective};
 use ark_ec::PrimeGroup;
 use ark_ff::UniformRand;
@@ -8,7 +9,41 @@ struct SignerNonce {
     nonce: G1Projective,
 }
 
+impl SignerNonce {
+    fn generate(index: u64) -> SignerNonce {
+        let mut rng = rand_core::OsRng;
+        let secret_nonce = Fr::rand(&mut rng);
+
+        let nonce = G1Projective::generator() * secret_nonce;
+
+        SignerNonce {
+            index,
+            secret_nonce,
+            nonce,
+        }
+    }
+}
+
 struct PartialSignature {
     index: u64,
-    private_key: Fr,
+    response: Fr,
+}
+
+impl PartialSignature {
+    pub fn sign(
+        nonce: &SignerNonce,
+        key_share: &Fr,
+        all_nonces: &[SignerNonce],
+        group_public_key: &G1Projective,
+        message: &[u8],
+    ) {
+        //first get the sum of all public nonces
+        let r = all_nonces[0].nonce;
+        for nonce in &all_nonces[1..] {
+            r + nonce.nonce;
+        }
+
+        //next, compute a challenge with the helper function for this nonce sum
+        let c = compute_challenge(&r, group_public_key, message);
+    }
 }
